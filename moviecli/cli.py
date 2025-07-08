40d2d93cf76608564eedafeb4b17a423062f0c54
yannickRafael <yannickrafael286@gmail.com>
2025-07-09 00:30:06 +0200

    added cli menu to choose movie from results

--- moviecli/cli.py
+++ moviecli/cli.py
@@ -1,11 +1,25 @@
 import click
 from ytswebs import *
+from utils import *
+from InquirerPy import inquirer
 
 @click.command
 @click.argument('movie')
 def search(movie):
     results = search_movie(movie)
-    click.echo(results)
+    choices, dict = format_search_results_into_choices(results)
+
+    selection = inquirer.select(
+        message="Select a movie:",
+        default=choices[0],
+        choices=choices,
+        pointer='=>',
+        instruction='Use arrow keys to navigate and Enter to select.',
+    ).execute()
+
+    click.secho(f'You selected {selection}', color="green")
+
+
 
 
 if __name__=='__main__':
