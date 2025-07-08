40916499fdc1ccbd2d365cd9759f079a39b5e9b4
yannickRafael <yannickrafael286@gmail.com>
2025-07-09 01:04:18 +0200

    fixed issue with wrong choices data type

--- moviecli/utils.py
+++ moviecli/utils.py
@@ -9,3 +9,27 @@ def format_search_results_into_choices(search_results):
         choices.append(choice)
         dict[choice] = url
     return choices, dict
+
+def format_movie_details(movie_details):
+    title = movie_details[0]
+    year = movie_details[1]
+    categories = movie_details[2]
+    tomatometer = movie_details[3]
+    audience = movie_details[4]
+    imdb = movie_details[5]
+
+    dict = movie_details[6]
+    choices = []
+    for key in dict.keys():
+        choices.append(key)
+
+    details = f'''
+Title: {title};
+Year: {year};
+Categories: {categories};
+Tomatometer: {tomatometer};
+Audience: {audience};
+IMDb: {imdb};
+options: {movie_details[6]}
+'''
+    return details, choices, dict
