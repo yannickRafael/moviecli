f29323ac4c0a6b1961b19b4e3408a275b353b162
yannickRafael <yannickrafael286@gmail.com>
2025-07-09 00:26:46 +0200

    refractor fuction to return choice-url map

--- moviecli/utils.py
+++ moviecli/utils.py
@@ -1,9 +1,11 @@
 def format_search_results_into_choices(search_results):
     choices = []
+    dict = {}
     for i, result in enumerate(search_results, start=1):
         title = result[0]
         url = result[1]
         year = result[3]
         choice = f'{i}. {title} ({year})'
         choices.append(choice)
-    return choices
+        dict[choice] = url
+    return choices, dict
