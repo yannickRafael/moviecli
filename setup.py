7b2ac505d00aa67f881cc52ecb416366489ef937
yannickRafael <yannickrafael286@gmail.com>
2025-07-09 02:00:43 +0200

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='moviecli',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     py_modules=['cli', 'torrent_downloader', 'utils', 'ytswebs', 'config'],
     install_requires=[
